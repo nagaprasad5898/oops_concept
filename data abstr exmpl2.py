3aea9ef62ce72b717550554adfbc1c98b4d35dec
kannalucky <nagaprasad5898@gmail.com>
2021-09-21 20:50:46 +0530

    hi

--- data abstr exmpl2.py
+++ data abstr exmpl2.py
@@ -1,1 +1,20 @@
-class
+class students():
+    def __init__(self,name,age):
+        self.__school="bashyam"
+        self.name=name
+        self.age=age
+        print("it is an students init")
+        self.__info() #calling the internal instance method in init with
+        # another name because to access this method when this
+        #class was using
+    def info(self):
+        print("i am in student class")
+    __info = info # assing the method name to another variable
+class teachers(students):
+    def __init__(self,name,age):
+        students.__init__(self,name,age)
+        print("it is an teachers init")
+    def info(self):
+        print("i am in teachers class")
+obj=teachers("kanna",25)
+obj.info()
