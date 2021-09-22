8f3b1e526c1e516730a1cebe404844c00c51aa4b
nagaprasad5898 <53175512+nagaprasad5898@users.noreply.github.com>
2021-09-22 17:37:19 +0530

    Update StringUpper.py

--- StringUpper.py
+++ StringUpper.py
@@ -3,8 +3,8 @@ class string:
         self.n=input("enter a string")
         return self.n
     def printstring(self):
-        self.n1=self.n.upper()
-        return self.n1
+        n1=self.n.upper()
+        return n1
 obj=string()
 print(obj.getstring())
 print(obj.printstring())
